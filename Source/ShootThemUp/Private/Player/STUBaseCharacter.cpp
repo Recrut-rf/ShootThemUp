0bc4d521b46fe64802e2c2dd11f674f471fc7092
Mihail Shiryaev <mihail.shiryaev@simbirsoft.com>
2025-06-27 12:45:00 +0400

    управление камерой персонажа

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -4,6 +4,7 @@
 #include "Player/STUBaseCharacter.h"
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
+#include <GameFramework/SpringArmComponent.h>
 
 
 // Sets default values
@@ -12,8 +13,12 @@ ASTUBaseCharacter::ASTUBaseCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+    SpringArmComponent->SetupAttachment(GetRootComponent());
+    SpringArmComponent->bUsePawnControlRotation = true;
+
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
-    CameraComponent->SetupAttachment(GetRootComponent());
+    CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
 // Called when the game starts or when spawned
@@ -37,6 +42,12 @@ void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASTUBaseCharacter::MoveForward);
     PlayerInputComponent->BindAxis("MoveRight", this, &ASTUBaseCharacter::MoveRight);
+        
+	//PlayerInputComponent->BindAxis("LookUp", this, &ASTUBaseCharacter::LookUp);
+    //PlayerInputComponent->BindAxis("TurnAround", this, &ASTUBaseCharacter::TurnAround);
+    // можно вот так забиндить напрямую
+    PlayerInputComponent->BindAxis("LookUp", this, &ASTUBaseCharacter::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("TurnAround", this, &ASTUBaseCharacter::AddControllerYawInput);
 
 }
 
@@ -49,4 +60,14 @@ void ASTUBaseCharacter::MoveRight(float Amount)
 {
     AddMovementInput(GetActorRightVector(), Amount);
 }
+
+//void ASTUBaseCharacter::LookUp(float Amount)
+//{
+//    AddControllerPitchInput(Amount);
+//}
+//
+//void ASTUBaseCharacter::TurnAround(float Amount)
+//{
+//    AddControllerYawInput(Amount);
+//}
 
